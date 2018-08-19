24d69b5bde33d414f407b796f8f5f0eab1365a50
Hypriot Pirate <pirate@black-pearl.localdomain>
2018-08-19 07:34:10 +0000

    Reduce verbosity

--- my_swaks.sh
+++ my_swaks.sh
@@ -8,17 +8,19 @@ do
  RETVAL=$?
  DATE=`date '+%Y-%m-%d %H:%M:%S'`
  if [ $RETVAL -eq 0 ]; then
-   echo Success at $DATE
+#   echo Success at $DATE
    if [ $ALERT_MAIL_HAS_BEEN_SENT -eq 0 ]; then
-     echo $SWAKS_ALERT_DOWN_CMD
+#     echo $SWAKS_ALERT_DOWN_CMD
+     echo Success at $DATE
      swaks $SWAKS_ALERT_DOWN_CMD --header "Subject: $SWAKS_ALERT_DOWN_SUBJECT" --body "$SWAKS_ALERT_DOWN_BODY"
      ALERT_MAIL_HAS_BEEN_SENT=1
    fi
    ALERT_MAIL_HAS_BEEN_SENT=1
  else
-   echo Failure at $DATE
+#   echo Failure at $DATE
    if [ $ALERT_MAIL_HAS_BEEN_SENT -eq 1 ]; then
-     echo $SWAKS_ALERT_UP_CMD
+#     echo $SWAKS_ALERT_UP_CMD
+     echo Failure at $DATE
      swaks $SWAKS_ALERT_UP_CMD --header "Subject: $SWAKS_ALERT_UP_SUBJECT" --body "$SWAKS_ALERT_UP_BODY"
      ALERT_MAIL_HAS_BEEN_SENT=0
    fi
